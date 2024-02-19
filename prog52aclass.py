e4dd36b0b7a3eca7a8f8f4aa6f1f891cd042a617
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-02-19 20:25:31 +0000

    2/19

--- prog52aclass.py
+++ prog52aclass.py
@@ -8,25 +8,25 @@ class Shape:
 
   # Mutator?setter: modifies class data
   def calculate(self):
-  self._area = self.length * self.width
-  self._perim = 2 * self.length + 2 * self.width
+    self._area = self.length * self.width
+    self._perim = 2 * self.length + 2 * self.width
 
-  def sefLength(self, length)
+  def setLength(self, length):
     self.length = length
 
   #Accessor/getter: returns class data
-def getarea(self):
-  return self._area
+  def getarea(self):
+    return self._area
 
-def detperim(self):
-  return self.perim
+  def getperim(self):
+    return self._perim
 
 
 def main():
-  len = int(input("Enter Length"))
-  wid = int(input("Enter Width"))
+  len = int(input("Enter Length: "))
+  wid = int(input("Enter Width: "))
   # make a new 'shape' object
-  shape = shape(len, wid) # call the shape constructor
+  shape = Shape(len, wid) # call the shape constructor
   # shape.setlength(5)
   shape.calculate()
   print("Area: ", shape.getarea())
