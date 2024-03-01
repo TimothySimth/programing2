8c20f2a34dbfa6852d05a16ecf2a824519b48cc1
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-03-01 20:34:39 +0000

    3/1

--- LP4-10while.py
+++
@@ -1,4 +0,0 @@
-import math
-num1 = int(input("Enter non negative num"))
-num2 = int(input("Enter non negative num"))
-print(math.gcd(num1, num2))
---
+++ LP5-10while.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter non negative num: "))
+num2 = int(input("Enter non negative num: "))
+
+while num2 > 0:
+  temp = num1 % num2
+  num1 = num2
+  num2 = temp
+
