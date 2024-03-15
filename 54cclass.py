3aaf61e122f29bd887aaed547a65e85c32f8ab46
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-03-15 19:35:08 +0000

    3/15

---
+++ 54cclass.py
@@ -0,0 +1,13 @@
+class circle:
+  
+  def __init__(self, radius):
+    self.radius = radius
+    self.pi = 3.14159
+  def calc():
+    
+
+  def getArea():
+    area = pi * radius**2
+
+  def getCircumference():
+    circ = 2 * pi * radius
