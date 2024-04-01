c4b5cde1f0dbd0003191a2ddc6d4a83e55b39623
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-04-01 19:18:18 +0000

    4/1

--- f.py
+++ f.py
@@ -1,10 +1,2 @@
-a = int(input("Enter non negative num :"))
-b = int(input("Enter non negative num :"))
-r = a%b
-q = int(a/b)
-while(r!=0):
-    a = b
-    b = r
-    q = int(a/b)
-    r = a - (b * q)
-print("GCD IS :", b)
+i = 0
+while i == 0:
