e5aacb7d3abcf3356fd23a312cd8d10a0a07cd1b
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-04-23 14:04:50 -0500

    4/23/24

---
+++ CSharp/LP4-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP4_2 {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter weight"); int weight = int.Parse(Console.ReadLine());
+            Console.Write("Enter length"); int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter width");  int width = int.Parse(Console.ReadLine());
+            Console.Write("Enter hight");  int hight = int.Parse(Console.ReadLine());
+
+            int volume = length * width * hight;
+
+            if (weight > 27 && volume > 100000) Console.WriteLine("Package is to heavy and to large");
+            else if (weight > 27)               Console.WriteLine("Package is to heavy");
+            else if (weight > 27)               Console.WriteLine("Package is to large");
+            else                                Console.WriteLine("Package is to Okay");
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ CSharp/Prog85c/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog85c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = num1 - 165;
+            double num3 = num2 / 100;
+            double num4 = Math.Round(num3);
+            
+
+
+        }
+    }
+}
