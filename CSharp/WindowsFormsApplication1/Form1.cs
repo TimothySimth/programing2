b2ddcda8fedc62c9c646cb8a1074909a665d1f8f
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-05-01 14:37:59 -0500

    5/1/2024

---
+++ CSharp/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            for (int x = 0; x < rand.Next(-12, 16); x++)
+            {
+                double Owen = Math.Pow(x, 3) - 3 * Math.Pow(x, 5) - 93 * Math.Pow(x, 4) + 87 * Math.Pow(x, 3) + 1596 * Math.Pow(x, 2) - 1380 * x - 2800;
+                listBox1.Items.Add(Owen);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
