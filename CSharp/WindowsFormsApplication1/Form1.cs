5641a808764438a3f6ad9312fc5ad12f7bb0705b
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-05-03 14:36:28 -0500

    5/3/2024

--- CSharp/WindowsFormsApplication1/Form1.cs
+++ CSharp/WindowsFormsApplication1/Form1.cs
@@ -19,10 +19,10 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int x = -12; x < -12; x++)
-            {
+            listBox1.Items.Add("X\t\tY");
+            for (int x = -12; x <= 16; x++) {
                 double Owen = Math.Pow(x, 3) - 3 * Math.Pow(x, 5) - 93 * Math.Pow(x, 4) + 87 * Math.Pow(x, 3) + 1596 * Math.Pow(x, 2) - 1380 * x - 2800;
-                listBox1.Items.Add(Owen);
+                listBox1.Items.Add(x + "\t\t" + Owen);
             }
 
         }
