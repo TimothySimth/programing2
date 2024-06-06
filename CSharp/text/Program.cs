fd7017e0786783ff0757b771f7dbe19fa3f22a00
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-06-06 13:09:04 -0500

    6/6/2024

---
+++ CSharp/text/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 80;
+            int b = 15;
+            int c = 2;
+            int g = a - b / (3 * c) * (a + c);
+            Console.WriteLine(g.ToString());
+            Console.ReadKey();
+        }
+    }
+}
